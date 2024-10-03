b2bd596fef61d396b7378bdd3ea224457c4629c1
Lucas Nascimento <lucasmnclima@gmail.com>
2024-10-03 13:58:56 -0300

    Migration, DbContext e Class.

---
+++ Gestao-de-salao/Migrations/20241003162607_M00.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gestao_de_salao.Migrations
+{
+    /// <inheritdoc />
+    public partial class M00 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Saloes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cnpj = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnoComeco = table.Column<int>(type: "int", nullable: false),
+                    AnoCadastro = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Saloes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Saloes");
+        }
+    }
+}
---
+++ Gestao-de-salao/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Gestao_de_salao.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+        public DbSet<Salao> Saloes { get; set; }
+    }
+}
