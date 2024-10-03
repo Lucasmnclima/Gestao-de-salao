7cd9198d99f07caea8999844cb284e9fae81c1dc
Lucas Nascimento <lucasmnclima@gmail.com>
2024-10-03 10:04:26 -0300

    Adicionar arquivos de projeto.

---
+++ Gestao-de-salao/Models/Salao.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gestao_de_salao.Models
+{
+    [Table("Saloes")]
+    public class Salao
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Cnpj { get; set; }
+        [Required]
+        public string Endereco { get; set; }
+        [Required]
+        public int AnoComeco { get; set; }
+        [Required]
+        public int AnoCadastro { get; set; }
+    }
+}
